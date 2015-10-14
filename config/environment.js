3d40bf072a508d5f9909e9564c659c1e6794b0a7
ankushgoyal146 <ankush.goyal32@gmail.com>
2015-10-14 11:28:48 +0530

    new Features

---
+++ config/environment.js
@@ -0,0 +1,55 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'violin-app',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+//  ENV.apiURL = "http://127.0.0.1:5000/api/";
+  if (environment === 'development') {
+    ENV.APP.LOG_TRANSITIONS = true;
+
+    ENV.contentSecurityPolicy = {
+      'connect-src': "'self' http://127.0.0.1:8000",
+      'style-src': "'self' 'unsafe-inline' *",
+      
+    }
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
---
+++ tests/unit/helpers/hour-min-sec-test.js
@@ -0,0 +1,10 @@
+import { hourMinSec } from '../../../helpers/hour-min-sec';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | hour min sec');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = hourMinSec(42);
+  assert.ok(result);
+});
